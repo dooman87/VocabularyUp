a980f23d1fa7a750f91746b49a45f5fab31f147d
Pokidov N. Dmitry <dooman87@gmail.com>
2009-09-11 16:30:33 +0400

    Add panel for test one word

---
+++ src/vocabularyup/ui/test/TestWordPanel.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vocabularyup.ui.test;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * Panel represents question in test.
+ *
+ * @author Pokidov.Dmitry
+ */
+public class TestWordPanel extends JPanel {
+    private String word;
+    private JTextField answer;
+
+    public TestWordPanel(String word) {
+        super();
+        this.word = word;
+        this.answer = new JTextField();
+        initUI();
+    }
+
+    protected void initUI() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JPanel labelPanel = new JPanel();
+        JLabel wordLabel = new JLabel(word);
+        wordLabel.setFont(wordLabel.getFont().deriveFont(20.0f));
+        labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.X_AXIS));
+        labelPanel.add(Box.createHorizontalGlue());
+        labelPanel.add(wordLabel);
+        labelPanel.add(Box.createHorizontalGlue());
+        add(labelPanel);
+
+        answer.setFont(answer.getFont().deriveFont(20.0f));
+        answer.setHorizontalAlignment(JTextField.CENTER);
+        add(answer);
+    }
+
+    public String getAnswer() {
+        return answer.getText();
+    }
+}
